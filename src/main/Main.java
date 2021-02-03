e4738260ee561e925d4ba637fe8eeda0f26fc8b4
Richard <richardkummer8@gmail.com>
2021-02-03 09:46:10 -0500

    Added File Selection functionality.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,19 @@
 package main;
 
+import java.util.Scanner;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import h_lang.*;
 
 public class Main {
-    public static void main(String[] args) { //TODO Add file selection
-        HVM hvm = new HVM(new HCompiler("content of hf file"));
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the path to file");
+        Path fileName = Path.of(input.nextLine());
+
+        HVM hvm = new HVM(new HCompiler(Files.readString(fileName)));
         hvm.run();
     }
 }
